49d7ebf92e3f9d199109ffc5e8cd99d65c732f1e
garyyufei <garyyufei@gmail.com>
2019-08-21 21:23:49 +1000

    add missing code

--- updown/modules/__init__.py
+++ updown/modules/__init__.py
@@ -1,5 +1,7 @@
 from .attention import BottomUpTopDownAttention
 from .updown_cell import UpDownCell
+from .CBS import ConstraintBeamSearch
+from .constraint import FreeConstraint
 
 
-__all__ = ["BottomUpTopDownAttention", "UpDownCell"]
+__all__ = ["BottomUpTopDownAttention", "UpDownCell", "ConstraintBeamSearch", "FreeConstraint"]
